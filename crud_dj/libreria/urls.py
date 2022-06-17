23fdf43a2f85165d730c4da8c9aa32795217dd39
edwbcruzv <73382784+edwbcruzv@users.noreply.github.com>
2022-06-17 15:31:58 -0500

    Subiendo...

---
+++ crud_dj/libreria/urls.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.inicio,name="inicio"),
+    path('nosotros',views.nosotros,name='nosotros'),
+    path('libros',views.libros,name="libros"),
+    path("crear",views.crear,name="crear"),
+    path("editar",views.editar,name="editar")
+]
---
+++ crud_dj/libreria/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+# Tempates es detectado automaticamente
+def inicio(request):
+    return render(request,"paginas/inicio.html")
+
+def nosotros(request):
+    return render(request,"paginas/nosotros.html") #Busca el archivo
+
+def libros(request):
+    return render(request,"libros/index.html")
+
+def crear(request):
+    return render(request,"libros/crear.html")
+
+def editar(request):
+    return render(request,"libros/editar.html")
